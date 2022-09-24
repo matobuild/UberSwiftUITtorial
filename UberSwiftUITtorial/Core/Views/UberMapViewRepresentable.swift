bb184ee14c581578b70ab16b74cf8aa0e102147a
Mato <tissuepaperarelimited@gmail.com>
2022-09-24 23:43:44 +0800

    -put map on main page

---
+++ UberSwiftUITtorial/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,45 @@
+//
+//  UberMapViewRepresentable.swift
+//  UberSwiftUITtorial
+//
+//  Created by kittawat phuangsombat on 2022/9/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension UberMapViewRepresentable {
+    
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+        
+        
+    }
+}
---
+++ UberSwiftUITtorial/learnWhat?.swift
@@ -0,0 +1,9 @@
+//
+//  learnWhat?.swift
+//  UberSwiftUITtorial
+//
+//  Created by kittawat phuangsombat on 2022/9/24.
+//
+
+//USE UIKIT in SWIFTUI
+//MAPKIT
