d3bc3a52bd9ac3138ec0590282d3a67744941719
Mato <tissuepaperarelimited@gmail.com>
2022-09-24 22:44:18 +0800

    Initial Commit

---
+++ UberSwiftUITtorial/UberSwiftUITtorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITtorialApp.swift
+//  UberSwiftUITtorial
+//
+//  Created by kittawat phuangsombat on 2022/9/24.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITtorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
